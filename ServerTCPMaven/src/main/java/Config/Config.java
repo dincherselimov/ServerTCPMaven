110edcd5d8b026142e4620ded4bf96cd5aeee94a
dincher <dinchitoo@gmail.com>
2021-10-13 16:00:09 +0300

    1

--- ServerTCPMaven/src/main/java/Config/Config.java
+++ ServerTCPMaven/src/main/java/Config/Config.java
@@ -19,7 +19,7 @@ public class Config {
     FileInputStream FIS;
     {
         try {
-            FIS = new FileInputStream("D:\\Java\\ServerTCP\\Server\\src\\ConfigFiles\\config.properties");
+            FIS = new FileInputStream("D:\\Manik\\ServerTCPMaven\\ServerTCPMaven\\src\\main\\java\\ConfigFiles\\config.properties");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
